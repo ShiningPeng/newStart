29d9f3aaec9f1add1aea671895dc0171f0e40f20
ShiningPeng <1494228462@qq.com>
2020-07-24 12:52:52 +0800

    update

--- react/sanyuan-music/src/routes/index.js
+++ react/sanyuan-music/src/routes/index.js
@@ -1,13 +1,28 @@
+
+import React, { lazy, Suspense } from 'react';
+import { Redirect } from 'react-router-dom';
+// import Recommend from '../application/Recommend/';
+// import Singers from '../application/Singers/';
+import BlankLayout from '../layouts/BlankLayout';
+import HomeLayout from '../layouts/HomeLayout';
+// 路由懒加载
+const RecommendComponent = lazy(() => import("../application/Recommend"))
+const SingersComponent = lazy(() => import("../application/Singers"))
+
+const SuspenseComponent = Component => props => {
+  // 延时加载组件
+  return (
+    <Suspense fallback={null}>
+      <Component {...props}></Component>
+    </Suspense>
+    )
+}
 // 路由的本质是什么？
 // 路由级别的页面
 // 二级路由 
 // /path Component 放在相应的地方<Route />
 // 嵌套
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import Recommend from '../application/Recommend/';
-import BlankLayout from '../layouts/BlankLayout';
-import HomeLayout from '../layouts/HomeLayout';
+
 
 export default [{
   component: BlankLayout,
@@ -23,12 +38,13 @@ export default [{
         },
         {
           path: '/recommend',
-          component: Recommend
+          component: SuspenseComponent(RecommendComponent)
         },
-        // {
-        //   path: '/singers',
-        //   component: SingersComponent
-        // },
+        {
+          path: '/singers',
+          component: SuspenseComponent(SingersComponent),
+          key:'singers'
+        },
         // {
         //   path: '/rank',
         //   component: RankComponent
