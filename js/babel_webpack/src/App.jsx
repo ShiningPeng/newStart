f571bed9dcb2dfb797c1a45541ddfb52836ca2ab
ShiningPeng <1494228462@qq.com>
2020-08-17 11:54:19 +0800

    babel and webpack

--- js/babel_webpack/src/App.jsx
+++ js/babel_webpack/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import style from './style.css'
+// 通过这样的方式引入图片
+// import img from './xxx.png'
+// 需要安装file-loader
+// 并且需要在webpack.config.js中配置module,rules下新建一个test
+// {
+//   test:/(.jpg|.png)$/,
+//   use:['file-loader']
+// }
+
 
 export default function App(){
   return(
-    <div>
+    // 放到服务器上时，地址
+    //  http://baidu.com/a/b/index.html
+    //  ./url.jpg => http://baidu.com/a/b/   与目标的文件地址不符合
+    //   /url.jpg => http://baidu.com/
+    <div 
+    // className="swiper-container"
+    className={style['swiper-container']}
+    >
+      <img src="/url.jpg" alt=""/>
       <h1>This is App</h1>
     </div>
   )
--- js/babel_webpack/webpack.config.js
+++ js/babel_webpack/webpack.config.js
@@ -1,8 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
+
 const config = {
   entry: './src/index.js',
   devServer: {
-    contentBase: "./dist",
-    hot: true
+    // contentBase是设置静态资源目录
+    // hot是热更新
+    contentBase: "./public",
+    inline: true
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -13,9 +19,38 @@ const config = {
         test: /(.js|.jsx)$/,
         use: ['babel-loader'],
         exclude: /node_modules/
+      },
+      {
+        test:/\.css$/i,
+        use:['style-loader',{
+          loader:'css-loader',
+          options:{
+            modules:{
+              mode:'local',
+              localIdentName:'[hash:base64:5]'
+            }
+          }
+        }]
+        // css-loader 解决import './style.css'语法，不然会报错
+        // style-loader的作用是将css内容插入到html的style标签内
+        // css-module
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:'./public/index.html',
+      filename:'index.html'
+    }),
+    new BundleAnalyzerPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './public/url.jpg', to: './' }
+      ],
+    }),
+
+  ]
+
 
 }
 
