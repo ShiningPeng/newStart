b297ac9eadb4f6fdf6e86f71943d8f5edd3aae80
ShiningPeng <1494228462@qq.com>
2020-08-25 12:02:32 +0800

    vue

---
+++ vue/element-code/README.md
@@ -0,0 +1,16 @@
+## vue-cli4.x  vue3变化太大了
+  - vue2.0 vue init webpack project-name 传统流行
+  - vue3.0 vue create project-name  
+  - ts
+  - PWA : 渐进式网页应用
+  - Router Vuex 全家桶
+  - CSS Pre-processors stylus vw autoprefixer
+    编译 sass/stylus 插件 autoprefixer 前缀
+    postcss-px-to-viewport
+  - linter 代码风格确保文件 ; "" '' tab
+  - eslint 保证大家代码风格一致
+      - 没用到的变量不要定义，要删除干净
+      - 对齐代码
+      - "" ''
+      - js 语句结尾不加分号
+  - unit Testing E2E 测试
---
+++ vue/element-code/src/components/ElButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+  @click="handleClick"
+    class="el-button"
+    :class="[
+      type?'el-button'+type:'',
+      {
+        'is-round':round,
+        'is-circle':circle
+      }
+    ]"
+  >
+    <!-- {{type}} - {{tit}} -->
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props:{
+    type:{
+      type:String,
+      default:'default' 
+    },
+    round:Boolean,
+    circle:Boolean
+  },
+  data(){
+    return {
+      tit:'hello'
+    }
+  },
+  methods: {
+    handleClick(evt){
+      this.$emit('handleClick',evt)
+    }
+  },
+  created(){
+    console.log(this.tit,this.type)
+  }
+
+}
+</script>
+
+<style lang="stylus">
+  
+</style>
